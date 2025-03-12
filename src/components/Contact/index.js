099e0c17fff0c14487f7876408d3cd9de0e18222
Ramanchi Rishi Sai Teja <102644611+xenon1919@users.noreply.github.com>
2025-03-12 20:56:32 +0530

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -182,10 +182,10 @@ const handleSubmit = (e) => {
         </Desc>
         <ContactForm ref={form} onSubmit={handleSubmit}>
           <ContactTitle>Email Me 🚀</ContactTitle>
-          <ContactInput placeholder="Your Email" name="from_email" />
-          <ContactInput placeholder="Your Name" name="from_name" />
-          <ContactInput placeholder="Subject" name="subject" />
-          <ContactInputMessage placeholder="Message" rows="4" name="message" />
+          <ContactInput placeholder="Your Email" name="from_email" required />
+          <ContactInput placeholder="Your Name" name="from_name" required />
+          <ContactInput placeholder="Subject" name="subject" required />
+          <ContactInputMessage placeholder="Message" rows="4" name="message" required />
           <ContactButton type="submit" value="Send" />
         </ContactForm>
         <Snackbar
